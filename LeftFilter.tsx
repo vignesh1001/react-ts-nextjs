442bc66e4adb19006e4723ab53ebeeecde44c6c5
vignesh1001 <vignesh1001@gmail.com>
2020-06-18 11:10:53 +0530

    1 file modified

--- LeftFilter.tsx
+++ LeftFilter.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Button from "@material-ui/core/Button";
 import { DialogActions, Box } from "@material-ui/core";
 import { Formik } from "formik";
@@ -18,7 +17,8 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import { Chip } from "@material-ui/core";
-
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
 const handleDelete = () => {
   alert("You clicked the delete icon.");
 };
@@ -47,10 +47,29 @@ const validationSchema = yup.object({
 
 const mystyle = {
   color: "black",
-  backgroundColor: "lightblue"
+  backgroundColor: "#00bfff"
 };
 
 export default function LeftFilter(props) {
+  const [state, setState] = useState({
+    filterTitle: "",
+    filterSkill: "",
+    filterTitleList: [],
+    filterSkillList: []
+  });
+  const handleChange = e => {
+    const { target, keyCode } = e;
+    const { name, value } = target;
+    setState({ ...state, [name]: value });
+  };
+  const handleKeyUp = e => {
+    const { target, keyCode } = e;
+    const { name, value } = target;
+    if (keyCode === 13) {
+      state[name + "List"].push(value);
+      setState({ ...state, [name]: "", [name + "List"]: state[name + "List"] });
+    }
+  };
   return (
     <Formik validationSchema={validationSchema} initialValues={initialValues}>
       {formikProps => {
@@ -66,6 +85,10 @@ export default function LeftFilter(props) {
                 Filter the job by title
               </InputLabel>
               <TextField
+                value={state.filterTitle}
+                name="filterTitle"
+                onKeyUp={handleKeyUp}
+                onChange={handleChange}
                 id="filterJOb"
                 aria-describedby="helper-text-forJOb"
                 fullWidth
@@ -77,18 +100,25 @@ export default function LeftFilter(props) {
                   )
                 }}
               />
-              <FormHelperText id="helper-text-forJOb" style={{paddingBottom:4}}>
+              <FormHelperText
+                id="helper-text-forJOb"
+                style={{ paddingBottom: 4 }}
+              >
                 Filter the job by title
               </FormHelperText>
-              <Button variant="contained" size="small" style={mystyle}>
-                Front end developres
-              </Button>
+              {state.filterTitleList.map(item => (
+                <Button variant="contained" size="small" style={mystyle}>
+                  {item}
+                </Button>
+              ))}
             </div>
-            
+
             <div>
-              <br />
-              <br />
               <TextField
+                value={state.filterSkill}
+                onKeyUp={handleKeyUp}
+                onChange={handleChange}
+                name="filterSkill"
                 id="filterSkill"
                 aria-describedby="my-helper-forSkill"
                 fullWidth
@@ -105,42 +135,30 @@ export default function LeftFilter(props) {
               </FormHelperText>
             </div>
             <br />
+            {state.filterSkillList.map(i => (
               <Chip
                 size="medium"
-                label="Javascript"
+                label={i}
                 onDelete={handleDelete}
                 style={mystyle}
               />
-              <Chip
-                size="medium"
-                label="CSS"
-                onDelete={handleDelete}
-                style={mystyle}
-              />
-              <Chip
-                size="medium"
-                label="HTML5"
-                onDelete={handleDelete}
-                style={mystyle}
-              />
-              <Chip
-                size="medium"
-                label="LERS"
-                onDelete={handleDelete}
-                style={mystyle}
-              />
-              <Chip
-                size="medium"
-                label="SASS"
-                onDelete={handleDelete}
-                style={mystyle}
-              />
-            <div style={{paddingTop:40}}>
+            ))}
+
+            <div style={{ paddingTop: 40 }}>
               <FormControl component="fieldset">
-                <FormLabel component="legend">
+                <FormLabel component="legend" style={{ color: "#00bfff" }}>
                   Filter by other job sites
                 </FormLabel>
-                <FormGroup aria-label="position">
+                <Avatar
+                  style={{
+                    backgroundColor: "orange",
+                    marginLeft: 180,
+                    marginTop: -25
+                  }}
+                >
+                  4
+                </Avatar>
+                <FormGroup aria-label="position" style={{ color: "#00bfff" }}>
                   <FormControlLabel
                     value="Dice"
                     control={<Checkbox color="primary" />}
